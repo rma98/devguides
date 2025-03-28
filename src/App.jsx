b7d188aed75b2b9d6d5a499ab7917be6cbd1cdbe
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-03-28 16:53:16 -0300

    Update setup inital for react projetc

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ReactGuide from "./pages/ReactGuide";
 import ReactNativeGuide from "./pages/ReactNativeGuide";
@@ -6,14 +6,17 @@ import VueGuide from "./pages/VueGuide";
 import ComandsGuide from "./pages/ComandsGuide";
 import ComandsGit from "./pages/ComandsGit";
 import About from "./pages/About";
+import CrossWordsPage from "./pages/CrossWordsPage";
 
-// Detecta se está rodando no GitHub Pages
 const isGitHubPages = import.meta.env.MODE === "production";
 
-function App() {
+function Layout() {
+  const location = useLocation(); // Obtém a URL atual
+
   return (
-    <Router basename={isGitHubPages ? "/meuprimeiroprojetoreactvite" : "/"}>
-      <Navbar />
+    <>
+      {/* Exibe a Navbar apenas se não estiver na página CrossWordsPage */}
+      {location.pathname !== "/crossWords" && <Navbar />}
       <Routes>
         <Route path="/" element={<ReactGuide />} />
         <Route path="/reactnativeguide" element={<ReactNativeGuide />} />
@@ -21,7 +24,16 @@ function App() {
         <Route path="/comandsguide" element={<ComandsGuide />} />
         <Route path="/comandsgit" element={<ComandsGit />} />
         <Route path="/about" element={<About />} />
+        <Route path="/crossWords" element={<CrossWordsPage />} />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router basename={isGitHubPages ? "/meuprimeiroprojetoreactvite" : "/"}>
+      <Layout />
     </Router>
   );
 }
