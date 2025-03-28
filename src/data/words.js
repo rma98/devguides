a370f63b3d082016421be7016ff909a1349c3290
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-03-28 16:52:19 -0300

    Add file words.js for data folder

---
+++ src/data/words.js
@@ -0,0 +1,14 @@
+const words = [
+    "PARAISO",
+    "SANTIDADE",
+    "EUCARISTIA",
+    "JESUS",
+    "GLORIA",
+    "CELESTE",
+    "FELICIDADE",
+    "PAZ",
+    "ESPIRITO",
+    "AMOR"
+];
+
+export default words;
