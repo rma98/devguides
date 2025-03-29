cb7c2ec37bbfe980af97764f62084c566482a249
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-03-28 22:30:38 -0300

    Update setup inital for react projetc

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ReactGuide from "./pages/ReactGuide";
 import ReactNativeGuide from "./pages/ReactNativeGuide";
